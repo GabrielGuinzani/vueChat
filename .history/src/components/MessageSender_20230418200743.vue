207ed8ed08afaabf306d051c8263b923d16cb55d
GabrielGuinzani <gferreiraguinzani@gmail.com>
2023-04-18 20:19:19 -0300

    Commit Secundário

---
+++ .history/src/components/MessageSender_20230418200743.vue
@@ -0,0 +1,23 @@
+<template>
+    <form id="form-chat-send" @submit.prevent="sendMessage">
+      <input v-model="message" type="text" id="message" name="message" placeholder="Digite sua mensagem..." class="typebar">
+      <button type="submit" class="botao">ENVIAR</button>
+    </form>
+  </template>
+  
+<script>
+export default {
+  name: 'MessageSender',
+  data() {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    sendMessage() {
+      this.$emit('message-sent', this.message);
+      this.message = '';
+    }
+  }
+}
+</script>
